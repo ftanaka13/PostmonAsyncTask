a543d74a8748ef895a6679ab0bbc667773cd844a
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-17 22:28:28 -0300

    parser manual

--- app/src/main/java/br/com/faculdadeimpacta/postmonasynctask/MainActivity.kt
+++ app/src/main/java/br/com/faculdadeimpacta/postmonasynctask/MainActivity.kt
@@ -2,11 +2,16 @@ package br.com.faculdadeimpacta.postmonasynctask
 
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.JsonReader
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.faculdadeimpacta.postmonasynctask.data.model.CEP
+import br.com.faculdadeimpacta.postmonasynctask.data.model.CidadeInfo
+import br.com.faculdadeimpacta.postmonasynctask.data.model.EstadoInfo
 import br.com.faculdadeimpacta.postmonasynctask.databinding.ActivityMainBinding
+import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -37,6 +42,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     inner class PostmonApi : AsyncTask<String, Void, String>() {
+        private var cep: CEP? = null
         override fun doInBackground(vararg p0: String?): String {
             val cepDigitado = p0[0]!!
 
@@ -46,14 +52,35 @@ class MainActivity : AppCompatActivity() {
                 conexao.requestMethod = "GET"
                 conexao.connect()
 
-                val stream = if (conexao.responseCode == HttpURLConnection.HTTP_OK) {
-                    conexao.inputStream
-                } else {
-                    conexao.errorStream
+                if (conexao.responseCode != HttpURLConnection.HTTP_OK) {
+                    return conexao.errorStream.reader().readText()
                 }
 
-                return stream.reader().readText()
+                val respostaString = conexao.inputStream.reader().readText()
+
+                val jsonResposta = JSONObject(respostaString)
+
+                cep = CEP(
+                    bairro = jsonResposta.getString("bairro"),
+                    cep = jsonResposta.getString("cep"),
+                    cidade = jsonResposta.getString("cidade"),
+                    cidade_info = CidadeInfo(
+                        area_km2 = jsonResposta.getJSONObject("cidade_info").getString("area_km2"),
+                        codigo_ibge = jsonResposta.getJSONObject("cidade_info")
+                            .getString("codigo_ibge")
+                    ),
+                    complemento = if (jsonResposta.has("complemento")) jsonResposta.getString("complemento") else null,
+                    estado = jsonResposta.getString("estado"),
+                    estado_info = EstadoInfo(
+                        area_km2 = jsonResposta.getJSONObject("estado_info").getString("area_km2"),
+                        codigo_ibge = jsonResposta.getJSONObject("estado_info")
+                            .getString("codigo_ibge"),
+                        nome = jsonResposta.getJSONObject("estado_info").getString("nome")
+                    ),
+                    logradouro = jsonResposta.getString("logradouro")
+                )
             }
+            return cep.toString()
         }
 
         override fun onPostExecute(result: String?) {
